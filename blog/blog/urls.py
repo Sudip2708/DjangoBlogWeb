c46aefdf722dd95e7340c736351551e315390e62
Dalibor Sova <daliborsova@seznam.cz>
2023-12-18 13:03:45 +0100

    • Ve složce projektu 'blog' otevřít settings.py • Nastavit cestu k složce templates: TEMPLATES... 'DIRS': [BASE_DIR / 'templates'] • Dopsat pod statiku odkazy na: MEDIA_URL, STATICFILES_DIRS, STATIC_ROOT, MEDIA_ROOT • Ve složce projektu 'blog' otevřít urls.py • Importovat settings a static: from django.conf import settings, from django.conf.urls.static import static • Zapsat kód pro zpřístupnění media a statiky z webu (když jsme v debug módu): if settings.DEBUG:... • V terminálu se přesunout do aplikace: $ cd blog • Vytvořit soubor pro migraci: $ python manage.py makemigrations • Migrovat projekt: $ python manage.py migrate

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,22 +1,39 @@
-"""
-URL configuration for blog project.
+### Soubor sloužící k k definování mapování URL adres (Uniform Resource Locators) na pohledy (views) v rámci aplikace ###
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Import modulů:
+'''
+django.contrib.admin: Tato část obsahuje funkcionality a nástroje pro administraci vaší webové aplikace.
+admin: v Django umožňuje snadnou tvorbu administrátorského rozhraní, kde můžete spravovat data uložená v databázi a další administrativní úkony
+django.urls: Tento modul poskytuje nástroje pro správu URL adres a routování v aplikaci.
+path: znamená, že se z této části používá funkce path, která slouží k definování cest URL a mapování na pohledy.
+from django.conf import settings: Tento import načítá modul settings z django.conf, který umožňuje přistupovat k nastavením Django, definovaným ve souboru settings.py
+from django.conf.urls.static import static: Tento import načítá funkci static z modulu django.conf.urls.static. Tato funkce je používána k definování URL cesty pro servírování statických souborů (včetně médií) v režimu vývoje.
+'''
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+
+# Definice URL cest a odpovídajícího mapování na pohledy
+'''
+'admin/': je cesta mapována na administrační rozhraní Django.
+'''
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+# Ověření vývojového režimu:
+# (Význam této podmínky spočívá ve změně chování aplikace v závislosti na tom, zda je aktivní vývojový režim nebo ne)
+# (V praxi to znamená, že při vývoji můžete přistupovat k těmto souborům přímo z webového prohlížeče, aniž byste je museli spravovat přes webserver. Ve výrobním prostředí by mělo být toto chování zakázáno, protože by mohlo představovat bezpečnostní rizika.)
+'''
+urlpatterns: je seznam obsahující definice URL tras v Django. Tyto trasy určují, jak mají být zpracovávány různé URL požadavky ve vaší aplikaci.
+settings.STATIC_URL: obsahuje URL pro statické soubory, například "/static/".
+settings.STATIC_ROOT: obsahuje cestu k adresáři, ve kterém jsou statické soubory uloženy na serveru.
+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT): přidává cestu pro servírování statických souborů.
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT): přidává cestu pro servírování mediálních souborů
+'''
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
